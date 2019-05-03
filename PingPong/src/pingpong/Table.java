9bd62ef027f75d0623cf307626c4d96291519378
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-03 17:22:43 -0400

    Add files via upload

--- PingPong/src/pingpong/Table.java
+++ PingPong/src/pingpong/Table.java
@@ -1,13 +1,59 @@
 /*
- * A table of a ping-pong game. 
+ * A table of a ping-pong game. Also, create the graphic components alongside the 
  */
 package pingpong;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  *
  * @author Syed Yousuf
  */
-public class Table 
+public class Table extends JPanel
 {
+    // Instance Variables
+    
+    
+    /*
+    * Default Constructor
+    */
+    public Table()
+    {
+        
+    }
+    
+    /*
+    * The paint method 
+    */
+    @Override 
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        
+        // Set a background colour
+        this.setBackground(Color.WHITE);
+        
+        g.setColor(Color.BLUE);
+        g.fillRect(240, 240, 200, 200);
+    }
+    /*
+    * A test method
+    */
+    public static void main(String[]args)
+    {
+        Table PongTable = new Table();
+        
+        // Create the frame 
+        JFrame f1 = new JFrame("Ping Pong");
+        f1.setSize(900,900);
+        f1.setVisible(true);
+        f1.add(PongTable);
+        
+    }
+    
+    
     
 }
