dab51dd69145de8fe23c2afd337d1cdec7753047
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-05 20:00:22 -0400

    Add files via upload

--- PingPong/src/pingpong/Ball.java
+++ PingPong/src/pingpong/Ball.java
@@ -3,21 +3,30 @@
  */
 package pingpong;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
 /**
  *
  * @author Syed Yousuf
  */
-public class Ball extends MoveableObject
+public class Ball extends JPanel
 {
     // Instance Variables
     private double radius;
+    private double xCoordinate;
+    private double yCoordinate;
+    
     
     /*
     * Constructor that creates an object with specific parameters 
     */
     public Ball(int xCoordinate, int yCoordinate, double radius)
     {
-        super(xCoordinate, yCoordinate);
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
         this.radius = radius;
         
     }
@@ -33,6 +42,80 @@ public class Ball extends MoveableObject
     }
     
     /*
+    * A getter method which gets the value of the x coordinate
+    *
+    *@return the x coordinate value of this moveable object
+    */
+    public double getXValue()
+    {
+        return xCoordinate;
+    }
+    
+    /*
+    * A getter method which gets the value of the y coordinate
+    *
+    *@return the y coordinate value of this moveable object
+    */
+    public double getYValue()
+    {
+        return yCoordinate;
+    }
+    
+    /*
+    * A setter method which sets the x coordinate value of the moveable object
+    *
+    *@param xCoordinate The new x coordinate value
+    */
+    public void setX(int xCoordinate)
+    {
+        this.xCoordinate = xCoordinate;
+    }
+    
+    /*
+    * A setter method which sets the y coordinate value of the moveable object
+    *
+    *@param yCoordinate The new y coordinate value
+    */
+    public void setY(int yCoordinate)
+    {
+        this.yCoordinate = yCoordinate;
+    }
+    
+    /*
+    * Move the moveable object in the x direction
+    *
+    *@param xDisplacement The amount of displacement the object must move in the x direction
+    */
+    public void moveX(int xDisplacement)
+    {
+        xCoordinate += xDisplacement;
+    } 
+    
+    /*
+    * Move the moveable object in the y direction
+    *
+    *@param yDisplacement The amount of displacement the object must move in the y direction
+    */
+    public void moveY(int yDisplacement)
+    {
+        yCoordinate += yDisplacement;
+    }
+    
+    
+    /*
+    * The override paint method which will paint this paddle.
+    */
+    @Override 
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        
+        g.setColor(Color.RED);
+        g.fillOval((int)xCoordinate, (int)yCoordinate, (int)(2 * radius), (int)(2 * radius));
+        
+    }
+    
+    /*
     * Providing a string representation of this class.
     *
     *@return The string representation of this class
@@ -41,6 +124,35 @@ public class Ball extends MoveableObject
     public String toString()
     {
         return "X: " + getX() + " Y: " + getY() + "Radius: " + radius;
+    }
+    
+    /*
+    * Test Method
+    */
+    public static void main(String[]args)
+    {
+        Ball p1 = new Ball(10,20, 50);
+        
+        JFrame f1 = new JFrame("Ping Pong");
+        f1.setSize(900,900);
+        f1.setVisible(true);
+        f1.add(p1);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        try 
+        {
+            Thread.currentThread().sleep(2000);
+        } catch (InterruptedException ie) 
+        {
+            //None.
+            
+        }
+        
+        p1.moveX(200);
+        
+        f1.validate();
+        f1.repaint();
+        
     }
     
 }
